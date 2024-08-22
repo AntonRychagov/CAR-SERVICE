1f2849fb83fab391c8d0735fc81ebeec226f0607
pauline13 <jesusdesu@yandex.ru>
2024-08-22 13:43:05 +0300

    fixed link in error page

--- src/pages/ErrorPage/ui/ErrorPage.tsx
+++ src/pages/ErrorPage/ui/ErrorPage.tsx
@@ -1,11 +1,12 @@
 import { Box, Link as MuiLink, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import { MainRoutePath } from '../../../shared/config/routeConfig/routeConfig';
 
 const ErrorPage = () => {
   return (
     <Box textAlign="center">
       <Typography pb={1}>Такой страницы не существует</Typography>
-      <MuiLink component={RouterLink} to={'/'} underline="none">
+      <MuiLink component={RouterLink} to={MainRoutePath.main} underline="none">
         Вернуться домой
       </MuiLink>
     </Box>
